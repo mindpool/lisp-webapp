926fa7db3960153c8192b208bcfe0d1b70d20392
Duncan McGreggor <duncan@dreamhost.com>
2012-09-20 23:59:58 -0700

    Small cleanups.

--- lib/global.lisp
+++ lib/global.lisp
@@ -1,14 +1,18 @@
 (in-package :webapp)
 
 ;;set the template directory
-(defvar *template-directory* (merge-pathnames "document-root/templates/" *webapp-home*))
+(defvar *template-directory*
+  (merge-pathnames "document-root/templates/" *webapp-home*))
 
 ;;and the static file path
-(defvar *static-web-files* (merge-pathnames "document-root/static/" *webapp-home*))
+(defvar *static-web-files*
+  (merge-pathnames "document-root/static/" *webapp-home*))
 
 ;;set the template/static paths (arguably, could also be done in 'pages'
 (setf html-template:*default-template-pathname* *template-directory*)
 
-(push 
- (hunchentoot:create-folder-dispatcher-and-handler "/static/" *static-web-files*)
+(push
+ (hunchentoot:create-folder-dispatcher-and-handler
+   "/static/"
+   *static-web-files*)
  hunchentoot:*dispatch-table*)
--- lib/pages.lisp
+++ lib/pages.lisp
@@ -3,7 +3,7 @@
 (hunchentoot:define-easy-handler (home :uri "/")
     ()
   (with-output-to-string (stream)
-    (html-template:fill-and-print-template 
+    (html-template:fill-and-print-template
      #p "welcome.htmlf"
      nil
      :stream stream)))
